5b589f0331856ae4f3fc81e5d47ec1e065ed74b6
Bill77 <billchen@aol.com>
2014-06-13 14:44:21 -0700

    Created simple widget directive example.

---
+++ app/js/Directives/widget.js
@@ -0,0 +1,13 @@
+app.directive('widget', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    template: '<div class="widgetTitle">{{widgetTitle}}</div><div class="widgetDetails" ng-transclude></div>',
+    scope: {
+      widgetTitle: '@'
+    },
+    link: function(scope, attrs) {
+      scope.widgetTitle = attrs.widgetTitle;
+    }    
+  }
+});
