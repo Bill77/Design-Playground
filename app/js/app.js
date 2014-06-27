ebfa1e5239c17d1c6f2f680ef1a748b58428137c
Bill Chen <bill.chen@elekta.com>
2014-06-27 16:57:31 -0700

    Created view 4 for testing controller/service communication Modified package.json to figure out iPad support

--- app/js/app.js
+++ app/js/app.js
@@ -15,5 +15,6 @@ app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.when('/view3', {templateUrl: 'partials/WebWidgetOrg.html'});
+  $routeProvider.when('/view4', {templateUrl: 'partials/patientList.html', controller: "PatientListCtrl", controllerAs: "PatientList"});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
